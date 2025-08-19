81965a5ec59b4d1b0486a37d892bed8aab970723
Ansh Gadhia <piec422@gmail.com>
2025-08-19 17:45:01 +0530

    Video error Fixed

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import BanVid from "/src/assets/bannervideo.mp4"
 
-const Video = () =>{
-    return (
-        <center>
-            <video controls width="75%" src={BanVid}></video>
-        </center>
-    );
-}
+const Video = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <video
+        className="rounded-2xl shadow-lg"
+        width="75%"
+        controls
+        autoPlay
+        loop
+        muted
+        playsInline
+      >
+        <source src="/bannervideo.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+};
 
 export default Video;
