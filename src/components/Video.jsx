ea1611865fabd48c56c7cb1653c62c7e3b3e731d
Ansh Gadhia <147709667+ansh-gadhia@users.noreply.github.com>
2025-08-19 14:46:46 +0530

    Update gdriveVideo.jsx

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import BanVid from "/bannervideo.mp4"
 
-const Video = () =>{
-    return (
-        <center>
-            <video controls width="75%" src={BanVid}></video>
-        </center>
-    );
-}
+const Video = () => {
+  return (
+    <div className="flex justify-center my-6">
+      <video
+        controls
+        width="75%"
+        className="rounded-2xl shadow-lg"
+        src="https://drive.google.com/uc?export=download&id=1wHuIJLdf7jc1tZLlHU-mq2Dn16Vc5VKQ"
+      >
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+};
 
 export default Video;
