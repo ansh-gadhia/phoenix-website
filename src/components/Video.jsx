4912aa030641fad4af3f27b912a8773017d04250
Ansh Gadhia <147709667+ansh-gadhia@users.noreply.github.com>
2025-08-19 14:53:38 +0530

    Update srcVideo.jsx

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,18 +1,12 @@
 import React from "react";
+import BanVid from "/src/assets/bannervideo.mp4"
 
-const Video = () => {
-  return (
-    <div className="flex justify-center my-6">
-      <video
-        controls
-        width="75%"
-        className="rounded-2xl shadow-lg"
-        src="https://drive.google.com/uc?export=download&id=1wHuIJLdf7jc1tZLlHU-mq2Dn16Vc5VKQ"
-      >
-        Your browser does not support the video tag.
-      </video>
-    </div>
-  );
-};
+const Video = () =>{
+    return (
+        <center>
+            <video controls width="75%" src={BanVid}></video>
+        </center>
+    );
+}
 
 export default Video;
